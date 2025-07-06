aa221b4c76ba695b46dc10f908d2f77004751df0
Mokete <ematyabekana@gmail.com>
2025-07-06 14:58:07 +0200

    Added UI_TEXT constants for buttons, cards, and messages

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,36 @@
-export const APP_NAME = "Alx Listing App";
-export const APP_DESCRIPTION = "A simple listing app built with Next.js and Tailwind CSS.";
+
+export const UI_TEXT={
+    BUTTON: {
+        SUBMIT: 'Submit',
+        CANCEL: 'Cancel',
+        DELETE: 'Delete',
+        EDIT: 'Edit',
+        ADD_TO_CART: 'Add to Cart',
+        VIEW_CART: 'View Cart',
+        SHOW_MORE: 'Show More',
+        SIGN_IN: 'Sign In',
+        SIGN_UP: 'Sign Up',
+        MORE_INFO: 'More Info',
+    },
+    CARD: {
+        TITLE:'Item Title',
+        PRICE: 'Price',
+        CITY: 'City',
+        IMAGE: 'Image',
+        NUMBER_OF_BEDS: 'Number of Beds',
+        NUMBER_OF_BATHS: 'Number of Baths',
+        NUMBER_OF_PEOPLE: 'Number of People',
+        RATING: 'Rating',
+        
+    },
+
+    ERROR_MESSAGES: {
+        REQUIRED_FIELD: 'This field is required.',
+        INVALID_EMAIL: 'Please enter a valid email address.',
+    },
+
+    SUCCESS_MESSAGES: {
+        ITEM_ADDED: 'Item successfully added to cart.',
+        ITEM_UPDATED: 'Item successfully updated.',
+    },
+}
