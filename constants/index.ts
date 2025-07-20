50890f2a739eb6d4182cb9137b972b332258d68f
Mokete <ematyabekana@gmail.com>
2025-07-20 16:23:49 +0200

    Created properties interface.

--- constants/index.ts
+++ constants/index.ts
@@ -457,3 +457,28 @@ export const PROPERTYLISTINGSAMPLE: PropertyProps []  =  [
     discount: ""
   }
 ];
+
+
+interface Address {
+  state: string;
+  city: string;
+  country: string;
+}
+
+interface Offers {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
+
+export interface PropertyProps {
+     name: string;
+  address: Address;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: Offers;
+  image: string;
+  discount: string;
+
+}
